dba4d60c2270ef178d9fa413af9502a4db89168c
LinskensLouis <90902367+LinskensLouis@users.noreply.github.com>
2023-11-15 21:10:09 +0100

    LuckyProject

--- LuckyBlock/src/layouts/default/AppBar.vue
+++ LuckyBlock/src/layouts/default/AppBar.vue
@@ -4,16 +4,22 @@
         <img class="logo" src="../../assets/logo.svg" width="30" height="30" />
         LuckyBlock
     </v-app-bar-title>
-    <v-btn variant="outlined" color="#80C9D4" class="btn" type="submit" href="">CGU</v-btn>
+    <v-btn variant="outlined" color="#80C9D4" class="btn" type="submit" href="https://epitechfr-my.sharepoint.com/:w:/g/personal/jules_toublant_epitech_digital/EX37-eglPopNlyFBzScRhHEBufJCe1VYoQ1KvgdqhZ7UKw?e=BhUQPQ">
+      Cookies & Vie privée
+    </v-btn>
+    <v-btn variant="outlined" color="#80C9D4" class="btn" type="submit" href=" https://epitechfr-my.sharepoint.com/:w:/r/personal/jules_toublant_epitech_digital/Documents/Conditions%20G%C3%A9n%C3%A9rales%20d.docx?d=w70ff8e9ee8554101912d13df91d29985&csf=1&web=1&e=C5Qt47">
+      CGU
+    </v-btn>
   </v-app-bar>
 </template>
-
+ 
 <script setup>
-  import Logo from '../../assets/logo.svg'
 </script>
-
+ 
 <style scoped>
-
+.btn{
+  margin-left: 10px;
+}
 .logo{
   margin-right: 10px;
 }
--- LuckyBlock/src/views/Home.vue
+++ LuckyBlock/src/views/Home.vue
@@ -1,8 +1,221 @@
-<script setup>
+<script>
+
 import BlockR from '../assets/blockr.svg'
 import BlockB from '../assets/blockb.svg'
 import BlockG from '../assets/blockg.svg'
+import Web3 from 'web3'
 
+document.addEventListener('DOMContentLoaded', async () => {
+    await chargerContrat();
+
+});
+
+let compte
+let adresseContrat = '0x22C107F58dcE8cb0cff66a637F593A16dEa44c15'; // Adresse réelle du contrat
+let abiContrat = [
+        {
+            "inputs": [],
+            "name": "effectuerTirage",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "envoyerFonds",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "participer",
+            "outputs": [],
+            "stateMutability": "payable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": false,
+                    "internalType": "address",
+                    "name": "gagnant",
+                    "type": "address"
+                }
+            ],
+            "name": "TirageEffectue",
+            "type": "event"
+        },
+        {
+            "inputs": [],
+            "name": "gagnant",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "montantDuTirage",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "nombreParticipants",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "name": "participants",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "proprietaire",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ]; // ABI réelle du contrat
+
+let web3 = new Web3(window.ethereum);
+let contrats = new web3.eth.Contract(abiContrat, adresseContrat);
+
+console.log("Contrat:", contrats);
+
+
+ 
+
+export default {
+  data() {
+    return {
+      items: [
+        { title: 'Block 1', src: BlockG, price : "0.001 ETH"},
+        { title: 'Block 2', src: BlockB, price : "0.002 ETH" },
+        { title: 'Block 3', src: BlockR, price : "0.005 ETH" },
+      ],
+        
+
+    };
+  },
+
+  mounted() {
+    this.chargerContrat();
+    this.participants();
+ 
+  },
+
+  methods: {
+
+    
+
+async chargerContrat() {
+    
+    // Charger le contrat
+    
+
+    const balance = await web3.eth.getBalance(adresseContrat);
+    console.log("Balance du contrat:", balance);
+    
+
+    // Vérifier si le compte est connecté à MetaMask
+    const comptes = await web3.eth.getAccounts();
+    compte = comptes.length > 0 ? comptes[0] : null;
+    if (compte === null) {
+        console.error("Veuillez vous connecter à MetaMask");
+    } else {
+        console.log("Compte MetaMask détecté:", compte);
+    }
+
+},
+
+async participants() {
+    let parts=contrats.methods.nombreParticipants().call({ from: adresseContrat});
+    console.log("Nombre de participants:", parts);
+},
+
+ async participer() {
+     try {
+        const participationTx = await contrats.methods.participer().send({ from: compte, value: web3.utils.toWei('0.001', 'ether') });
+        console.log(participationTx);
+       
+     } catch (error) {
+         console.error("Erreur lors de la participation:", error);
+     }
+},
+
+
+ async effectuerTirage() {
+     try {
+         const tirageTx = await contrats.methods.effectuerTirage().send({ from: compte });
+         console.log(tirageTx);
+         
+     } catch (error) {
+         console.error("Erreur lors de l'effetuation du tirage:", error);
+     }
+ },
+
+ async envoyerFonds() {
+     try {
+         const envoiFondsTx = await contrats.methods.envoyerFonds().send({ from: compte });
+         console.log(envoiFondsTx);
+
+     } catch (error) {
+         console.error("Erreur lors de l'envoi des fonds:", error);
+     }
+ }
+
+  },
+
+  
+};
 </script>
 
 <template>
@@ -29,52 +242,32 @@ import BlockG from '../assets/blockg.svg'
       </v-carousel-item>
   </v-carousel>
 
-  <div class = "infos">
-    <v-banner
-      lines="one"
-      icon="mdi-clock-alert"
-      class="my-4"
-    >
-      <v-banner-text>
-        Date du tirage : 24/09/2029
-      </v-banner-text>
 
-    </v-banner>
 
-    <v-banner
-      lines="two"
-      icon="mdi-account"
-      class="my-4"
-    >
-      <v-banner-text>
-         Nombre de participants : 50 personnes
-      </v-banner-text>
-    </v-banner>
-
-  </div>
 
   <v-btn
+    @click="participer"
       variant="flat"
       color="#5063BF"
       class="btn"
-    >
-      Participer
-    </v-btn>
+    >Participer</v-btn>
+    <v-btn
+    @click="effectuerTirage"
+      variant="outlined"
+      color="#5063BF"
+      class="btn"
+    >Tirage gagnant</v-btn>
+    <v-btn
+    @click="envoyerFonds"
+      variant="outlined"
+      color="#5063BF"
+      class="btn"
+    >Envoi Gains</v-btn>
+
+
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      items: [
-        { title: 'Block 1', src: BlockG, price : "0.001 ETH"},
-        { title: 'Block 2', src: BlockB, price : "0.002 ETH" },
-        { title: 'Block 3', src: BlockR, price : "0.005 ETH" },
-      ],
-    };
-  },
-};
-</script>
+
 
 <style scoped>
 .car-image {
